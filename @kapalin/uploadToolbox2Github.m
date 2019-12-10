6222dbbf80e5210d49b347e7f86755099f654334
gitlab@srinivas.gs <code@srinivas.gs>
2019-12-10 15:16:43 -0500

    switched to better version numbering

--- @kapalin/uploadToolbox2Github.m
+++ @kapalin/uploadToolbox2Github.m
@@ -41,9 +41,11 @@ end
 
 desc = ['"Click on the file named ' options.github_binary{1} ' to download a MATLAB toolbox. Drag that file onto your MATLAB workspace to install. This release has been generated using kapalin, an automated testing framework in MATLAB."'];
 
+t = datevec(today);
+version_name = ['version ' mat2str(t(1)-2000) '.' mat2str(t(2)) '.' mat2str(3)];
 
 % create a release 
-[e,o]=system(['github-release release --user ' options.github_user_name ' --repo ' options.github_repo ' --tag ' datestr(today) ' --name ' datestr(today) ' -s ' github_token ' --description ' desc]);
+[e,o]=system(['github-release release --user ' options.github_user_name ' --repo ' options.github_repo ' --tag ' datestr(today) ' --name ' version_name ' -s ' github_token ' --description ' desc]);
 
 % upload the binary 
 [e,o]=system(['github-release upload --user ' options.github_user_name ' --repo ' options.github_repo ' --tag ' datestr(today) ' --name ' options.github_binary{1} ' -s ' github_token ' --file ' options.github_binary{1}]);
